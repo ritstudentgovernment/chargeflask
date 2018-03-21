45c748d268a344d62a61f19899cadce4c38876f0
Chris Lemelin <clemelin2@gmail.com>
2018-03-21 13:23:28 -0400

    Tests (#7)
    Fix tests

--- app/invitations/controllers.py
+++ app/invitations/controllers.py
@@ -61,15 +61,17 @@ def send_invite(new_user, committee):
             app_url= app.config['CLIENT_URL'] + str(invitation.id)
         )
 
-        # Attach sglogo to email.
-        with app.open_resource("static/sg-logo.png") as fp:
-            msg.attach("static/sg-logo.png", "image/png", fp.read(), headers={'Content-ID': '<sg-logo>'})
+        if not app.config['TESTING']:
 
-        # Attach paw to email.
-        with app.open_resource("static/paw.png") as fp:
-            msg.attach("static/paw.png", "image/png", fp.read(), headers={'Content-ID': '<sg-paw>'})
+            # Attach sglogo to email.
+            with app.open_resource("static/sg-logo.png") as fp:
+                msg.attach("static/sg-logo.png", "image/png", fp.read(), headers={'Content-ID': '<sg-logo>'})
 
-        mail.send(msg)
+            # Attach paw to email.
+            with app.open_resource("static/paw.png") as fp:
+                msg.attach("static/paw.png", "image/png", fp.read(), headers={'Content-ID': '<sg-paw>'})
+
+            mail.send(msg)
         return Response.InviteSent
     except Exception as e:
 
@@ -78,7 +80,7 @@ def send_invite(new_user, committee):
 
 
 ##
-## @brief      Sends a request email to join a committee 
+## @brief      Sends a request email to join a committee
 ##             to the committee head.
 ##
 ## @param      new_user   The user to be added.
@@ -103,37 +105,37 @@ def send_request(new_user, committee):
         committee_id = committee.id,
         isInvite= False
     )
-    
+
     try:
 
         db.session.add(invitation)
-        db.session.commit() 
+        db.session.commit()
 
         msg = Message("Great news, " + new_user.id + " wants to join!")
         msg.sender = ("SG TigerTracker", "sgnoreply@rit.edu")
         msg.recipients = [committee.head + "@rit.edu"]
         msg.subtype = "related"
         msg.html = render_template(
-            'committee_request.html', 
+            'committee_request.html',
             user_name= new_user.id,
             committee_head= committee.head,
             committee_name= committee.title,
             time_stamp= time.time(),
             request_url= app.config['CLIENT_URL'] + str(invitation.id)
         )
+        if not app.config['TESTING']:
+            # Attach sglogo to email.
+            with app.open_resource("static/sg-logo.png") as fp:
+                msg.attach("static/sg-logo.png", "image/png", fp.read(), headers={'Content-ID': '<sg-logo>'})
 
-        # Attach sglogo to email.
-        with app.open_resource("static/sg-logo.png") as fp:
-            msg.attach("static/sg-logo.png", "image/png", fp.read(), headers={'Content-ID': '<sg-logo>'})
+            # Attach paw to email.
+            with app.open_resource("static/paw.png") as fp:
+                msg.attach("static/paw.png", "image/png", fp.read(), headers={'Content-ID': '<sg-paw>'})
 
-        # Attach paw to email.
-        with app.open_resource("static/paw.png") as fp:
-            msg.attach("static/paw.png", "image/png", fp.read(), headers={'Content-ID': '<sg-paw>'})
-
-        mail.send(msg)
+            mail.send(msg)
         return Response.RequestSent
     except Exception as e:
-        
+
         db.session.rollback()
         return Response.RequestError
 
@@ -198,7 +200,7 @@ def get_invitation(user_data):
 ##
 @socketio.on('set_invitation')
 def set_invitation(user_data):
-    
+
     invitation = Invitations.query.filter_by(id = user_data["invitation_id"]).first()
     user = Users.verify_auth(user_data["token"])
 
@@ -241,7 +243,8 @@ def set_invitation(user_data):
         }
 
         from app.members.controllers import add_to_committee
-        add_to_committee(add_data)
+        returnValue = add_to_committee(add_data)
+        print("got here")
 
         emit("set_invitation", Response.InviteAccept)
     else:

597790440ca304495ff7cc3134f39aa997cc2d3c
zujko <peter@zujko.xyz>
2018-03-30 15:55:08 -0400

    Implement Docker

--- config.py
+++ config.py
@@ -11,19 +11,19 @@ DEBUG = True
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 #Define the databae URI (Postgres)
-SQLALCHEMY_DATABASE_URI = secrets.SQLALCHEMY_DATABASE_URI
-SQLALCHEMY_TEST_DATABASE_URI = secrets.SQLALCHEMY_TEST_DATABASE_URI
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', '')
+SQLALCHEMY_TEST_DATABASE_URI = os.environ.get('SQLALCHEMY_TEST_DATABASE_URI', '')
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Secret key for signing tokens
-SECRET_KEY = secrets.APP_SECRET_KEY
+SECRET_KEY = os.environ.get('APP_SECRET_KEY', '')
 
 # Email configuration
-MAIL_SERVER = 'mymail.ad.rit.edu'
+MAIL_SERVER = os.environ.get('MAIL_HOST', 'mymail.rit.edu')
 MAIL_PORT = 465
-MAIL_USERNAME = secrets.MAIL_USERNAME
-MAIL_PASSWORD = secrets.MAIL_PASSWORD
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME', '')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', '')
 MAIL_USE_TLS = True
 
 # WebClient URL (For email support, client: https://git.io/vFDqH)

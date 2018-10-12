4a2327e2f7d2986f88085d5c23f316d772bb41f2
Omar De La Hoz <omar.dlhz@hotmail.com>
2018-10-12 15:14:56 -0400

    Chargetracker-9 (#29)
    * Fixed Sentry bug CHARGETRACKER-9
    
    * Fixed Sentry bug CHARGETRACKER-8

---
+++ app/users/users_response.py
@@ -0,0 +1,9 @@
+"""
+filename: users_response.py
+description: Responses for users module.
+created by: Omar De La Hoz (oed7416@rit.edu)
+created on: 10/12/18
+"""
+
+class Response():
+	AuthError = {'error': 'Authentication error.'}

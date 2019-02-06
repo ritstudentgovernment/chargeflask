6d6ee0dda6c81b41959c320861716744179f18f1
Omar De La Hoz <omar.dlhz@hotmail.com>
2019-02-06 16:23:03 -0500

    Added Topics to models

--- app/minutes/models.py
+++ app/minutes/models.py
@@ -10,8 +10,14 @@ class Minutes(db.Model):
 	__tablename__ = 'minutes'
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	title = db.Column(db.String)
-	topics = db.Column(db.String)
+	topics = db.relationship("Topics")
 	date = db.Column(db.DateTime)
-	body = db.Column(db.String)
-	committee_id = db.Column(db.String, db.ForeignKey('committee.id'))
+	committee_id = db.Column(db.String, db.ForeignKey('committees.id'))
 	committee = db.relationship("Committee", back_populates="minutes")
+
+
+class Topics(db.Model):
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	topic = db.Column(db.String)
+	body = db.Column(db.String)
+	minute_id = db.Column(db.Integer, db.ForeignKey('minutes.id'))

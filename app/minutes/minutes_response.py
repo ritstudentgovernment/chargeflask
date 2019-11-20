f4c90a70b09c999366730ef9dd09e9c4432325e5
Omar De La Hoz <omar.dlhz@hotmail.com>
2019-11-19 20:21:00 -0500

    Made changes to minutes

--- app/minutes/minutes_response.py
+++ app/minutes/minutes_response.py
@@ -17,7 +17,7 @@ class Response():
     AddMinuteSuccess = {"success": "Minute has been added to committee."}
     AddMinuteError = {"error": "Minute couldn't be added to committee."}
     MinuteDoesntExist = {"error": "Minute doesn't exist."}
-    EditSuccess = {"error": "Minute successfully edited."}
+    EditSuccess = {"success": "Minute successfully edited."}
     EditError = {"error": "Minute couldn't be edited."}
     DeleteMinuteSuccess =  {"error": "Minute successfully deleted."}
     DeleteMinuteError =  {"error": "Minute couldn't be deleted."}
